d4ead78451e2d2a95d2fd1817161916ad7598741
Vijayan Balasubramanian <balasvij@amazon.com>
2021-01-06 13:45:47 -0800

    [k-NN Plugin] Add controller
    controller to call gateway methods.

--- controller/knn/knn.go
+++ controller/knn/knn.go
@@ -1,1 +1,44 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package knn
+
+import (
+	"context"
+	"odfe-cli/gateway/knn"
+)
+
+//go:generate go run -mod=mod github.com/golang/mock/mockgen  -destination=mocks/mock_knn.go -package=mocks . Controller
+
+//Controller is an interface for the AD plugin controllers
+type Controller interface {
+	GetStatistics(context.Context, string, string) ([]byte, error)
+}
+
+type controller struct {
+	gateway knn.Gateway
+}
+
+//GetStatistics gets stats data based on nodes and stat names
+func (c controller) GetStatistics(ctx context.Context, nodes string, names string) ([]byte, error) {
+	return c.gateway.GetStatistics(ctx, nodes, names)
+}
+
+//New returns new Controller instance
+func New(gateway knn.Gateway) Controller {
+	return &controller{
+		gateway,
+	}
+}
--- controller/knn/knn_test.go
+++ controller/knn/knn_test.go
@@ -1,1 +1,51 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package knn
+
+import (
+	"context"
+	"errors"
+	"odfe-cli/gateway/knn/mocks"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestController_GetDistinctValues(t *testing.T) {
+	t.Run("gateway failed", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockGateway := mocks.NewMockGateway(mockCtrl)
+		ctx := context.Background()
+		mockGateway.EXPECT().GetStatistics(ctx, "", "").Return(nil, errors.New("gateway failed"))
+		ctrl := New(mockGateway)
+		_, err := ctrl.GetStatistics(ctx, "", "")
+		assert.Error(t, err)
+	})
+	t.Run("get stats success", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		mockGateway := mocks.NewMockGateway(mockCtrl)
+		ctx := context.Background()
+		mockGateway.EXPECT().GetStatistics(ctx, "node1", "stats").Return([]byte(`response succeeded`), nil)
+		ctrl := New(mockGateway)
+		result, err := ctrl.GetStatistics(ctx, "node1", "stats")
+		assert.NoError(t, err)
+		assert.EqualValues(t, []byte(`response succeeded`), result)
+	})
+}
